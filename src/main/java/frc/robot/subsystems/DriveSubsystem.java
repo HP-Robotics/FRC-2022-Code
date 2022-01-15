be8e4960d4d6cce6e9f635edc5207625f0628fdb
Lil Mac <scottie@hprobotics.org>
2022-01-15 12:50:05 -0600

    editing drive train for 4 motors

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -7,16 +7,22 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class DriveSubsystem extends SubsystemBase {
-    public TalonSRX Left1;
-    public TalonSRX Right1;
+    public TalonFX Left1;
+   public TalonFX Right1;
+   public TalonFX Left2;
+   public TalonFX Right2;
     public DriveSubsystem(){
 
-    Left1 = new TalonSRX(11);
-    Right1 = new TalonSRX(12);
+    Left1 = new TalonFX(1);
+    Right1 = new TalonFX(3);
+    Left2 = new TalonFX(2);
+    Right2 = new TalonFX(4);
 }
 
-    public void drive(double left1, double right1){
-        Left1.set(ControlMode.PercentOutput, left1);
-        Right1.set(ControlMode.PercentOutput, right1);
+    public void drive(double left, double right){
+        Left1.set(ControlMode.PercentOutput, left);
+       Right1.set(ControlMode.PercentOutput, right);
+       Left2.set(ControlMode.PercentOutput, left);
+       Right2.set(ControlMode.PercentOutput, right);
 }
 }
