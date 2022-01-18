777ce0669e2511a9b0d730e6fe7aa67992488b9f
Lil Mac <scottie@hprobotics.org>
2022-01-18 16:23:02 -0600

    Working Drive Base for FallBot

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -9,20 +9,20 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class DriveSubsystem extends SubsystemBase {
     public TalonFX Left1;
    public TalonFX Right1;
-   public TalonFX Left2;
-   public TalonFX Right2;
+   // public TalonFX Left2;
+  // public TalonFX Right2;
     public DriveSubsystem(){
 
-    Left1 = new TalonFX(1);
-    Right1 = new TalonFX(3);
-    Left2 = new TalonFX(2);
-    Right2 = new TalonFX(4);
+    Left1 = new TalonFX(12);
+    Right1 = new TalonFX(11);
+   // Left2 = new TalonFX(2);
+   // Right2 = new TalonFX(4);
 }
 
     public void drive(double left, double right){
         Left1.set(ControlMode.PercentOutput, left);
        Right1.set(ControlMode.PercentOutput, right);
-       Left2.set(ControlMode.PercentOutput, left);
-       Right2.set(ControlMode.PercentOutput, right);
+   // Left2.set(ControlMode.PercentOutput, left);
+    //   Right2.set(ControlMode.PercentOutput, right);
 }
 }
