cb9fa7c8168273d69c94052e90129e0eb30b1e56
HP Robotics Driver Station <scottie@hprobotics.org>
2022-01-25 19:41:35 -0600

    Basic magazine code

--- src/main/java/frc/robot/commands/MagazineToggleCommand.java
+++ src/main/java/frc/robot/commands/MagazineToggleCommand.java
@@ -4,19 +4,25 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.MagazineSubsystem;
 
 public class MagazineToggleCommand extends CommandBase {
-    private final MagazineSubsystem MagazineSubsystem;
-    private boolean active;
+    private final MagazineSubsystem m_subsystem;
 
-    public MagazineToggleCommand(MagazineSubsystem subsytem, boolean active) {
-        MagazineSubsystem = subsytem;
-        this.active = active;
+    public MagazineToggleCommand(MagazineSubsystem subsystem) {
+        m_subsystem = subsystem;
 
-        addRequirements(MagazineSubsystem);
+        addRequirements(subsystem);
     }
 
     @Override
+    public void initialize() {
+        if (m_subsystem.toggled == true){
+            m_subsystem.toggled = false;
+        }
+        else {
+            m_subsystem.toggled = true;
+        }
+    }
     public void execute() {
-        
+
     }
 
     @Override
