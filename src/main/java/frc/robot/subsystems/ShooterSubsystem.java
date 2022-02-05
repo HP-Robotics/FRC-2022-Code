58ca520d424e5462be16c85453e22fa5f8253140
Lil Mac <scottie@hprobotics.org>
2022-02-05 13:37:22 -0600

    Shooter Rumble

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems;
+ package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
 
@@ -18,36 +18,58 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ShooterSubsystem extends SubsystemBase {
 
-    public double WheelSetPoint;
+    public double WheelSetPoint = 0;
     private ShuffleboardTab tab = Shuffleboard.getTab("Shooter Configuration");
     private NetworkTableEntry InputSpeed = tab.add("Input Speed", Constants.shooterSpeed)
             .getEntry();
     public TalonFX m_shooter;
     public TalonFX PreShooter;
+    public double lRumble = 0;
+    public double rRumble = 0;
 
     public ShooterSubsystem() {
-        /*
-         * m_shooter = new TalonFX(21);
-         * m_shooter.configFactoryDefault();
-         * m_shooter.config_kP(0, Constants.shooterkP);
-         * m_shooter.config_kI(0, Constants.shooterkI);
-         * m_shooter.config_kD(0, Constants.shooterkD);
-         * m_shooter.config_kF(0, Constants.shooterkF);
-         * PreShooter = new TalonFX(22);
-         * 
-         * SmartDashboard.putNumber("shooterkP", Constants.shooterkP);
-         * SmartDashboard.putNumber("shooterkI", Constants.shooterkI);
-         * SmartDashboard.putNumber("shooterkD", Constants.shooterkD);
-         * SmartDashboard.putNumber("shooterkF", Constants.shooterkF);
-         */
+
+        m_shooter = new TalonFX(21);
+        m_shooter.configFactoryDefault();
+        m_shooter.config_kP(0, Constants.shooterkP);
+        m_shooter.config_kI(0, Constants.shooterkI);
+        m_shooter.config_kD(0, Constants.shooterkD);
+        m_shooter.config_kF(0, Constants.shooterkF);
+        PreShooter = new TalonFX(22);
+
+        SmartDashboard.putNumber("shooterkP", Constants.shooterkP);
+        SmartDashboard.putNumber("shooterkI", Constants.shooterkI);
+        SmartDashboard.putNumber("shooterkD", Constants.shooterkD);
+        SmartDashboard.putNumber("shooterkF", Constants.shooterkF);
+
     }
 
     public void periodic() {
-        // m_shooter.set(ControlMode.Velocity,WheelSetPoint);
+        if (WheelSetPoint == 0){
+            m_shooter.set(ControlMode.PercentOutput,0);
+        }
+         else{
+            m_shooter.set(ControlMode.Velocity,WheelSetPoint);
+        }
+        m_shooter.getSelectedSensorVelocity();
+        
+        if (WheelSetPoint ==  0){
+            lRumble = 0;
+            rRumble = 0;
+
+        }
+        else if  (Math.abs(WheelSetPoint - m_shooter.getSelectedSensorVelocity()) <100){
+            rRumble = 0;
+            lRumble = 0.8;
+        }
+        else {
+            lRumble = 0; 
+            rRumble = 0.8;
+        }
     }
 
     public void shoot(double speed) {
-        // PreShooter.set(ControlMode.PercentOutput,speed);
+        PreShooter.set(ControlMode.PercentOutput,speed);
     }
 
     public void Enable(boolean WheelOn) {
