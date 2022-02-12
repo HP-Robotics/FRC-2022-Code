c3c5103d78ab73ca4715bfbed61a4294c7373295
HP Robotics Driver Station <scottie@hprobotics.org>
2022-02-12 15:29:59 -0600

    Remove Rumble Error (Temporary)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -38,7 +38,7 @@ import frc.robot.subsystems.ShooterSubsystem;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  private Boolean m_useShooter = true;
+  private Boolean m_useShooter = false;
   private Boolean m_useClimber = false;
   private Boolean m_useIntake = false;
   private Boolean m_useDrive = false;
